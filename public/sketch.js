8cac9cd1d297f4960a005bd468901bd03b386a69
110318 <andrescamilogomezamaya@gmail.com>
2022-08-09 16:12:26 -0500

    API'S URL

--- public/sketch.js
+++ public/sketch.js
@@ -1,4 +1,17 @@
 let canvas;
+let randomUserURL = 'https://randomuser.me/api/'
+let catURL = 'https://catfact.ninja//fact'
+let usaURL = 'https://datausa.io//api/data?drilldowns=Nation&Measures=Population'
+let bitcoinURL = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+let petURL = 'https://dog.ceo/api/breeds/image/random'
+
+let respondRandomuser = null;
+let catfact = null
+let usPopulation = null
+let bitcoinPrice = null
+let petImage = null
+let imageData = null
+let img = null
 
 function setup() {
     frameRate(60);
@@ -7,12 +20,41 @@ function setup() {
     canvas.style('position', 'fixed');
     canvas.style('top', '0');
     canvas.style('right', '0');
+
+
+
+    console.log(fetch(URL).then(response => response.json()))
+    //function getRandomuser(){}
+    let content  = "Random User\r\n"
+    fetch(randomUserURL)
+    .then(response => response.json())
+    .then(data=> {data.results.forEach(user => {
+        content += 'NAME' + user.name.first+ ' '+ user.name.last + '\r\n'
+
+        
+    });
+        console.log(content.results[0])
+    })
+
+    //function petImage(){}
+    fetch(petURL)
+    .then(response => response.json())
+    .then(data => {
+        img=null
+        responseData =nu
+    })
 }
 
 function draw() {
     //background(0, 50);
     background(0);
     newCursor();
+    if(content != null){
+        textSize(20);
+        textWrap(WORD)
+        text(content.results[0],50,50,100)
+    }
+  
     
 }
 
